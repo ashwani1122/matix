2c740506a06f7b5d07f64867301ff4f4f4f2e788
ashwani1122 <ashwanisingh3846@gmail.com>
2026-04-19 11:42:36 +0530

    added multiplayer logic

--- index.ts
+++ index.ts
@@ -1,55 +1,95 @@
 import { readFileSync } from "fs";
 
 const html = readFileSync("./index.html", "utf-8");
-
-function generate(): number[] {
-  const numbers: number[] = [];
+type Player = {
+  id: string;
+  score: number;
+};
+let serverRef: Bun.Server<WSData>;
+let players = new Map<string, Player>();
+let currentNumbers: number[] = [];
+let currentSum = 0;
+function generateGame() {
+  currentNumbers = [];
 
   for (let i = 0; i < 3; i++) {
-    numbers.push(Math.ceil(Math.random() * 100));
+    currentNumbers.push(Math.ceil(Math.random() * 100));
   }
 
-  return numbers;
+  currentSum = currentNumbers.reduce((a, b) => a + b, 0);
 }
-
-Bun.serve({
+function broadcast() {
+  serverRef.publish(
+    "game",
+    JSON.stringify({
+      type: "game_update", // ✅ match frontend
+      numbers: currentNumbers,
+      players: Array.from(players.values()),
+    })
+  );
+}
+type WSData = {
+  id: string;
+};
+generateGame();
+Bun.serve<WSData>({
   port: 3000,
 
   fetch(req, server) {
-    if (server.upgrade(req)) return;
+  serverRef = server; // ✅ store reference
 
-    return new Response(html, {
-      headers: { "Content-Type": "text/html" },
-    });
-  },
+  if (server.upgrade(req, {
+    data: {
+      id: crypto.randomUUID(),
+    },
+  })) return;
+
+  return new Response(html, {
+    headers: { "Content-Type": "text/html" },
+  });
+},
 
   websocket: {
-    open(ws) {
-      ws.send(JSON.stringify({
-        type: "numbers",
-        data: generate(),
-      }));
-    },
+  open(ws) {
+  const id = ws.data.id; // ✅ use existing
+
+  players.set(id, { id, score: 0 });
+
+  ws.subscribe("game");
+
+  ws.send(JSON.stringify({
+    type: "game_update",
+    numbers: currentNumbers,
+    players: Array.from(players.values()),
+  }));
+},
 
     message(ws, message) {
       if (typeof message !== "string") return;
 
-      try {
-        const parsed = JSON.parse(message);
+      const parsed = JSON.parse(message);
 
-        if (parsed.type === "generate") {
-          ws.send(JSON.stringify({
-            type: "numbers",
-            data: generate(),
-          }));
-        }
-      } catch (err) {
-        console.log("Invalid JSON");
+     if (parsed.type === "answer") {
+      if(!ws.data){
+        return
       }
+  const player = players.get(ws.data.id);
+
+  if (!player) return;
+
+  if (parsed.value === currentSum) {
+    player.score++;
+
+    generateGame();
+    broadcast(); // ✅ no ws.server
+  } else {
+    ws.send(JSON.stringify({ type: "wrong" }));
+  }
+}
     },
 
     close(ws) {
-      console.log("Client disconnected");
+      players.delete(ws.data.id);
     },
   },
 });
